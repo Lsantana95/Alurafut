2e82c565dd8165be1aca337c0b1149f6ccaad740
Leonardo <leonardosantanav95@gmail.com>
2021-09-21 10:46:23 -0400

    alurafut

---
+++ index.js
@@ -0,0 +1,20 @@
+var campeonatos = [
+
+    "https://img.ibxk.com.br/2021/08/06/06151145565036.jpg",
+    "https://espnpressroom.com/brazil/files/2020/08/UEFA-Europa-League-3-780x470.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/f/f9/UEFA-Konfrans-Liqas%C4%B1.jpg",
+    "https://www.turistafc.com.br/wp-content/uploads/2020/09/blog-tabelapremier-destaque.jpg",
+    "https://a.espncdn.com/combiner/i?img=/i/leaguelogos/soccer/500/10.png",
+    "https://www.lance.com.br/files/article_main/uploads/2020/03/12/5e6a278ae5569.jpeg",
+    "https://www.minhatorcida.com.br/imagens/post/13134/tim.jpg",
+    "https://brasfutebol.com/wp-content/uploads/2021/06/ligue-1.jpg",
+    "https://cdn.falauniversidades.com.br/wp-content/uploads/2021/04/29121755/Libertadores-2021.jpg",
+    "https://cdn.falauniversidades.com.br/wp-content/uploads/2021/05/24163256/Copa-Sul-Americana.jpg",
+    "https://tntsports.com.br/__export/1592412188749/sites/esporteinterativo/img/2020/06/17/brasileira-primeira-rodada_00079062_0.jpg_1902800913.jpg",
+    "http://bet-ibc.com/wp-content/uploads/Argentina-Primera-Division-via-BET-IBC.png"
+
+]
+
+for (var i = 0; i < campeonatos.length; i++) {
+    document.write("<img src=" + campeonatos[i] + ">" )
+}
